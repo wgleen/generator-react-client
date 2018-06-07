7452a6568bfd77bfb99a62b4f657ce84b536f181
Wallace Gleen <wallace.gleen@gmail.com>
2018-06-07 11:06:45 -0300

    fix app.development import

--- app/templates/src/server/index.js
+++ app/templates/src/server/index.js
@@ -1,7 +1,7 @@
 import config from './config'
 import logger from './lib/logger'
 
-const app = require(`./app.${config.env == 'development' ? 'development' : 'production'}.js`).index
+const app = require(`./app.${config.env == 'development' ? 'development' : 'production'}.js`).default
 
 export const server = app.listen(config.port, err => {
   if (err) logger.error(err)
